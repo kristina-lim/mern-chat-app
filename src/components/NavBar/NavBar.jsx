7cc5d8331f2ffc8fb9abc326c1c164a6dde5c6cf
kristina-lim <kristinahlim@gmail.com>
2022-12-14 14:41:26 -0800

    Add pages and implement bootstrap

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,20 +1,84 @@
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import * as userService from '../../utilities/users-service';
+import {
+  MDBNavbar,
+  MDBContainer,
+  MDBIcon,
+  MDBNavbarNav,
+  MDBNavbarItem,
+  MDBNavbarLink,
+  MDBNavbarToggler,
+  MDBNavbarBrand,
+  MDBCollapse
+} from 'mdb-react-ui-kit';
 
 export default function NavBar({ user, setUser }) {
+  const [showNavColor, setShowNavColor] = useState(false);
+  const [showNavColorSecond, setShowNavColorSecond] = useState(false);
+  const [showNavColorThird, setShowNavColorThird] = useState(false);
+  
   function handleLogOut() {
     userService.logOut();
     setUser(null);
   }
-  
+
   return (
-    <nav>
-      <Link to="/orders">Order History</Link>
-      &nbsp; | &nbsp;
-      <Link to="/orders/new">New Order</Link>
-      &nbsp; | &nbsp;
-      <span>Welcome, {user.name}</span>
-      &nbsp;&nbsp;<Link to="" onClick={handleLogOut}>Log Out</Link>
-    </nav>
+    <>
+      <MDBNavbar expand='lg' light style={{ backgroundColor: '#FFF8E1' }}>
+        <MDBContainer fluid>
+          <MDBNavbarBrand href='#'>
+            <img
+              src='https://i.imgur.com/LVWGHBr.gif'
+              height='90'
+              width='90'
+              alt=''
+              loading='lazy'
+            />
+          </MDBNavbarBrand>
+          <MDBNavbarToggler
+            type='button'
+            data-target='#navbarColor02'
+            aria-controls='navbarColor02'
+            aria-expanded='false'
+            aria-label='Toggle navigation'
+            onClick={() => setShowNavColorThird(!showNavColorThird)}
+          >
+            <MDBIcon icon='bars' fas />
+          </MDBNavbarToggler>
+          <MDBCollapse show={showNavColorThird} navbar>
+            <MDBNavbarNav className='me-auto mb-2 mb-lg-0'>
+              <MDBNavbarItem className='active'>
+                <MDBNavbarLink aria-current='page' to='/profile'>
+                  Profile
+                </MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink to='#'>Features</MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink href='/chat'>Chat</MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink href='' onClick={handleLogOut}>Log Out</MDBNavbarLink>
+              </MDBNavbarItem>
+            </MDBNavbarNav>
+          </MDBCollapse>
+        </MDBContainer>
+      </MDBNavbar>
+    </>
   );
 }
+
+  
+//   return (
+//     <nav>
+//       <Link to="/orders">Order History</Link>
+//       &nbsp; | &nbsp;
+//       <Link to="/orders/new">New Order</Link>
+//       &nbsp; | &nbsp;
+//       <span>Welcome, {user.name}</span>
+//       &nbsp;&nbsp;<Link to="" onClick={handleLogOut}>Log Out</Link>
+//     </nav>
+//   );
+// }
--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -3,8 +3,8 @@ import { Routes, Route } from 'react-router-dom';
 import { getUser } from '../../utilities/users-service';
 import './App.css';
 import AuthPage from '../AuthPage/AuthPage';
-import NewOrderPage from '../NewOrderPage/NewOrderPage';
-import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
+import HomePage from '../HomePage/HomePage';
+import ChatPage from '../ChatPage/ChatPage';
 import NavBar from '../../components/NavBar/NavBar';
 
 export default function App() {
@@ -17,8 +17,8 @@ export default function App() {
           <NavBar user={user} setUser={setUser} />
           <Routes>
             {/* route components in here */}
-            <Route path="/orders/new" element={<NewOrderPage />} />
-            <Route path="/orders" element={<OrderHistoryPage />} />
+            <Route path="/" element={<HomePage />} />
+            <Route path="/chat" element={<ChatPage />} />
           </Routes>
         </>
         :
